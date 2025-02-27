1f589d46a8bdffbef3d45832091503fbe4c4207b
MUKILTEO\1721807 <https://github.com/Dback4248>
2025-02-27 13:35:12 -0800

    Added mario prefab and colliders

--- 2D Template/Assets/Scrips/PlayerMovement.cs
+++ 2D Template/Assets/Scrips/PlayerMovement.cs
@@ -5,18 +5,18 @@ public class Playermovement : MonoBehaviour
 {
 
 	public float movementSpeed;
-    
-    private Rigidbody2D _rb;
+
+	private Rigidbody2D _rb;
 	private object _moveAmount;
 
 	void Awake()
-    {
-        // Set the _rb variable equal to this GameObject's rigidbody.
-        _rb = GetComponent<Rigidbody2D>();
-    }
+	{
+		// Set the _rb variable equal to this GameObject's rigidbody.
+		_rb = GetComponent<Rigidbody2D>();
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 		_rb.linearVelocityX = _moveAmount.x * movementSpeed;
 	}
 }
